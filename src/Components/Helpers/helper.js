291e0712d7f0b8ff28a0456f55fa4768baaa7d3b
juan <marcecinco@fpuna.edu.py>
2022-11-07 21:44:32 -0300

    cambio dominio nuevamente

--- src/Components/Helpers/helper.js
+++ src/Components/Helpers/helper.js
@@ -1,12 +1,12 @@
 /**declaramos las apis correspondientes y asignamos su dirección correspondiente*/
-const URL_AUTH_USER = 'https://brxsgo.com/api/auth/login'
-const USER = 'https://brxsgo.com/api/user/'
-const ROLES = "https://brxsgo.com/api/roles/"
-const TICKETS = "https://brxsgo.com/api/tickets/"
-const MINING_MACHINES = "https://brxsgo.com/api/mining/"
-const MACHINES = "https://brxsgo.com/api/machines/"
-const CLIENT = 'https://brxsgo.com/api/client/'
-const ASSIGNROLE = "https://brxsgo.com/api/roles/roleadd/"
+const URL_AUTH_USER = 'https://backend.brxsgo.com/api/auth/login'
+const USER = 'https://backend.brxsgo.com/api/user/'
+const ROLES = "https://backend.brxsgo.com/api/roles/"
+const TICKETS = "https://backend.brxsgo.com/api/tickets/"
+const MINING_MACHINES = "https://backend.brxsgo.com/api/mining/"
+const MACHINES = "https://backend.brxsgo.com/api/machines/"
+const CLIENT = 'https://backend.brxsgo.com/api/client/'
+const ASSIGNROLE = "https://backend.brxsgo.com/api/roles/roleadd/"
 
 /**exportamos nuestras declaraciones de valores
  * para poder utilizarlas en donde se requieran
